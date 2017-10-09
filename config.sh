12b55f9efd0eb928173a3525a053971d11dc5a55
Karla-Isabel-Sandoval <k.isabel.sandoval@gmail.com>
2017-10-09 15:12:26 -0700

    comment out access key and secret

---
+++ config.sh
@@ -0,0 +1,12 @@
+export HTTP_PORT=3000
+export APPLICATION_NAME="connectwtwitterapi"
+
+
+var client = new Twitter({
+  consumer_key: 'Hu9HBjMMx6ElzbfszAYLcWBMe',
+  consumer_secret: 'cYR29ROnOf9354tWVpMCvQCMYXu80lHAkqUgiwg9cW5GUZo8EU',
+  bearer_token: 'AAAAAAAAAAAAAAAAAAAAAEn12AAAAAAAi4fXupHOh%2FriBKb13lfG%2B20P%2FwY%3DC2T3w99ER7g4xjCx1GOumItM0WsrcwWlINlzKrB96yhmoSUklo'
+});
+
+#access_token_key: '2668517719-UTrrcdDyzG9pi0OienyPCeYan9YcWERe22VH3vx',
+#access_token_secret: 'ngDKasMdSLaOtuacfpzCHd0BU4z7SYRYArR4PYLVmO7y7',
